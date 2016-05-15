b170b81f9ab6954faf3af666774fbcd4166e150d
renato <renato.a.r.goncalves>
2016-05-15 18:02:39 -0300

    Initial commit

---
+++ app/src/main/java/desenvolvimento_android/myanimationapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package desenvolvimento_android.myanimationapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        textView.setAnimation(anim);
+    }
+}
